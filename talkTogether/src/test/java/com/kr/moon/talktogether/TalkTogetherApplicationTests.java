384fe1ea76bc3fcfafbc620b80a425d69409f5a3
munjo.sung <moonjo960@gmail.com>
2024-02-25 23:36:42 -0500

    init project

---
+++ talkTogether/src/test/java/com/kr/moon/talktogether/TalkTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kr.moon.talktogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkTogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
